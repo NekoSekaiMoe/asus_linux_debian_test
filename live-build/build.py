785b4f7086d906201761e362cbfcc3126b6f6557
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-07-18 00:03:48 +0800

    add iso build

---
+++ live-build/build.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import subprocess
+import os
+import sys
+
+def run_command(command):
+    stderr=subprocess.STDOUT
+    process = subprocess.Popen(command,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True,
+                               universal_newlines=True)
+    while process.poll() is None:
+        line = process.stdout.readline().strip()
+        if line:
+            print(line)
+
+    return process.returncode
+
+def clone_repo():
+    command = "git clone https://github.com/NekoSekaiMoe/asus_linux_debian_test ci --depth=1"
+    run_command(command)
+
+
+def build_repo():
+    if os.path.exists(/deb)
+        command = "mv /deb/*.deb ci/live-build/config/packages.chroot/ && cd ci/live-build && lb config && lb build"
+    else
+        command = "cd ci/live-build && lb config && lb build"
+    run_command(command)
+
+
+def main():
+    clone_repo()
+    build_repo()
+
+
+if __name__ == '__main__':
+    main()
